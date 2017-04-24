78ffe8413b98a711de86821eb6c39f51eac64a00
Lenovo <Lenovo@LENOVO->
2017-04-24 18:52:00 +0300

    Добавьте файлы проекта.

---
+++ Парсинг Башо/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+using System.Net;
+
+
+
+namespace Парсинг_Башо
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(new WebClient().DownloadString("http://bash.im/random"));
+
+            HtmlNodeCollection result = doc.DocumentNode.SelectNodes("//div[@class='text']");
+
+            foreach(HtmlNode node in result)
+            {
+                Console.WriteLine(node.InnerText);
+                Console.WriteLine("***************");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
